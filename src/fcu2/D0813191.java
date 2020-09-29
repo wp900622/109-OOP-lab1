db6825caf66a2bcb769303b5f3dda94eb73a070e
user <user@EB09>
2020-09-29 09:34:59 +0800

    這是第二版

--- src/fcu2/D0813191.java
+++ src/fcu2/D0813191.java
@@ -4,7 +4,9 @@ public class D0813191 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("I am ¤ý¼yÀM");
+		System.out.println("My nid is D0813191");
+		System.out.println("This is lab1 hw");
 	}
 
 }
